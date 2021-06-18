763d913c4900276ce4b3369cca942a5734633072
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-06-18 12:58:16 +0530

    Set Input and Output Data

--- app/src/main/java/com/yugal/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/yugal/workmanagerdemo1/MainActivity.kt
@@ -2,14 +2,17 @@ package com.yugal.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val KEY_COUNT_VALUE = "key_count"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +24,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOneTimeWorkRequest(){
         val workManager = WorkManager.getInstance(applicationContext)
+
+        val data: Data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE,125)
+            .build()
         val constraints = Constraints.Builder()
             .setRequiresCharging(true)
             .setRequiredNetworkType(NetworkType.CONNECTED)
@@ -28,11 +35,18 @@ class MainActivity : AppCompatActivity() {
 
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .setConstraints(constraints)
+            .setInputData(data)
         .build()
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
                 textView.text = it.state.name
+                if(it.state.isFinished){
+                    val data: Data = it.outputData
+                    val message = data.getString(UploadWorker.KEY_WORK)
+                    Toast.makeText(applicationContext, message.toString(),Toast.LENGTH_LONG).show()
+
+                }
             })
     }
 }
